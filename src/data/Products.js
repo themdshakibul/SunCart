01481390b86f37a4fa3ae54204d4f0bf55c218de
themdshakibul <mdshakibul88240@gmail.com>
2026-04-30 16:53:36 +0600

    deploy problem solved

--- src/data/Products.js
+++ src/data/Products.js
@@ -1,6 +1,7 @@
 export const getproducts = async () => {
-  // const res = await fetch("http://localhost:3000/product.json");
-  const res = await fetch("https://sun-cart-bay.vercel.app/product.json", { cache: "no-store" });
+  const res = await fetch("https://sun-cart-seven.vercel.app/product.json", {
+    cache: "no-cache",
+  });
   const data = await res.json();
   return data;
 };
