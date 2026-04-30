28ce70d5d34ec21b79f17301eeced12272704819
themdshakibul <mdshakibul88240@gmail.com>
2026-04-30 16:34:41 +0600

    chang fetch url

--- src/data/Products.js
+++ src/data/Products.js
@@ -1,6 +1,6 @@
 export const getproducts = async () => {
   // const res = await fetch("http://localhost:3000/product.json");
-  const res = await fetch("https://sun-cart-bay.vercel.app/product.json");
+  const res = await fetch("https://sun-cart-bay.vercel.app/product.json", { cache: "no-store" });
   const data = await res.json();
   return data;
 };
