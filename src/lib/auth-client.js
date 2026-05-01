0469a15c901b50d951db2ec9663f866cc73e0144
themdshakibul <mdshakibul88240@gmail.com>
2026-05-01 11:41:57 +0600

    auth client url change

--- src/lib/auth-client.js
+++ src/lib/auth-client.js
@@ -1,7 +1,7 @@
 import { createAuthClient } from "better-auth/react";
 export const authClient = createAuthClient({
-  /** The base URL of the server (optional if you're using the same domain) */
-  baseURL: "http://localhost:3000",
+  // baseURL: "http://localhost:3000",
+  baseURL: "https://sun-cart-seven.vercel.app",
 });
 
 export const { signIn, signUp, signOut, useSession } = createAuthClient();
